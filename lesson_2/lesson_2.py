b652947b3ea1336553e976fda51afa2b19870958
Ilya <polyak.i.2506@gmail.com>
2024-10-22 20:31:28 +0300

    HW

---
+++ lesson_2/lesson_2.py
@@ -0,0 +1,123 @@
+# int -> ціле число 12
+# float -> дробове число 12.5
+# bool -> логічний тип даних: True False
+# str -> рядок - масив (набір) символів
+
+# Змінна - це іменована область оперативної пам'яті значення можна змінювати
+# number: int = 10 + 2
+# print(number)
+# print(type(number))
+# number = 20.5
+# print(number)
+# print(type(number))
+# number = "Vasya"
+# print(number)
+# print(type(number))
+# number = True
+# print(number)
+# print(type(number))
+#
+# a1 = 10
+# a2 = 20
+# first_number = 10
+# second_number = 20
+# #
+# userName1 = "Vasya"
+# print(userName1)
+# user_name = "Petya"
+# print(user_name)
+#
+# NUMBER = 10
+#
+# name = "Vasya"
+# age = 33
+
+# user_name = name + str(age)
+# print(user_name)
+
+###############
+# number1 = 10
+# print(number1)
+# print(number := 10 + 1)  # моржовый оператор
+
+##############
+# операторы: математические и логические
+# унарные - может быть только один операнд (например -5) тут минус унарный
+# бинарные - работают с двумя операндами (например 2 + 2)
+# операнд - это значение над которым производит операцию оператор (математический или логический)
+
+# + - * /
+# print(2 + 3)
+# print(2 - 3)
+# print(2 * 3)
+# print(2 / 3)
+# print(2 ** 3)  # основа ** показник (зведення в ступінь)
+# print(2 // 3)  # ціла частина від розподілу
+# print(2 % 3)  # залишок від ділення
+# #
+# num1 = 15
+# num2 = 8
+# print(num1 // num2)
+# print(num1 % num2)
+# print(-4)
+
+# ctrl + /
+
+#######
+# ввести з клавіатури тризначне число та вивести суму цифр цього числа
+# // %
+
+# 274 => 2 + 7 + 4
+
+# number = 123
+# n1 = number // 100
+# # v1
+# n2 = number // 10 % 10
+# # v2
+# # n2 = number % 100 // 10
+# n3 = number % 10
+#
+# print(n1)
+# print(n2)
+# print(n3)
+#
+# result = n1 + n2 + n3
+# print("Result: ", result)
+
+####
+# input -> буде очікувати на введення даних з клавіатури в
+# консолі і поверне за замовчуванням значення типу даних str
+
+# first_number = int(input("Enter first number: "))
+# second_number = int(input("Enter second number: "))
+# result = first_number + second_number
+# print(result)
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# # v1
+# print("Hello,", name, "You are", age, "years old!")
+# # v2 конкатенація - складання рядків. Рядок + рядок -> один великий рядок
+# print("Hello, " + name + " You are " + str(age) + " years old!")
+# # v3 інтерполяція рядка - вбудовування змінних у рядок завдяки функції format (вивчимо докладніше пізніше)
+# print(f"Hello, {name}. You are {age} years old!")
+#
+# age_after_ten_years = age + 10
+# print(age_after_ten_years)
+
+#####
+# number = int(input('Enter a number: '))  # 56198
+#
+# n1 = 5
+# n2 = 6
+# n3 = 1
+# n4 = 9
+# n5 = 8
+#
+# result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+# print(result)
+
+# merge vs rebase
+# https://www.youtube.com/watch?v=d5rvy5XPyzk
+
+
