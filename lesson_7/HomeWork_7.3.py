2f278ba06700e27cfdf27beec6b7cdedc46ca611
Ilya <polyak.i.2506@gmail.com>
2024-11-03 10:57:15 +0200

    HW

---
+++ lesson_7/HomeWork_7.3.py
@@ -0,0 +1,12 @@
+def second_index(text, some_str):
+
+    first_index = text.find(some_str)
+
+    if first_index == -1:
+        return None
+
+    second_index = text.find(some_str, first_index + 1)
+
+    return second_index if second_index != -1 else None
+
+print(second_index("Hello, hello", "lo"))
